18a32f57ae3e4aa951c2f22db81ae8baa35fad3d
Chian-Yu <72839899+Chian-Yu@users.noreply.github.com>
2021-03-09 23:46:54 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+alert("This web page is used to collect polygon and point vector and attribute data. The drawing toolbox is on the left side. The pentagon button is used to draw polygon. The marker button is used to draw points. The pen button is for edit and the garbage can is for delete. After finishing your feature, please enter the feature's title and its description.")
+
+var map = L.map('map').setView([23.96, 120.48], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hpYW55dSIsImEiOiJja2hjcG5ubXUwMXZ4Mnp0OW93enk5Yjh2In0.PgRvkVtqOFh9ew6lp-BFuw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,7 +8,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: '{Your Access Token Goes Here}'
 }).addTo(map);
 
 var drawnItems = L.featureGroup().addTo(map);
